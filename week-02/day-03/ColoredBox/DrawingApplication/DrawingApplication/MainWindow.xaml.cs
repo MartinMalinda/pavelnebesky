46b407c5bf3f8d2dc02fa89a1a5a8c40729239b0
Pavel Nebesky <pavel@nebesky.eu>
2019-09-18 14:47:06 +0200

    First two drawing exercises

---
+++ week-02/day-03/ColoredBox/DrawingApplication/DrawingApplication/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+using System;
+using System.Collections.Generic;
+
+namespace DrawingApplication
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+
+            //here
+            // Draw a box that has different colored lines on each edge.
+
+            var dict = new Dictionary<Color, List<int>>()
+            {
+                { Colors.BurlyWood, new List<int> () { 150, 50, 250, 50}},
+                { Colors.Green, new List<int> () { 250, 50, 250, 150} },
+                { Colors.Blue, new List<int> () { 250, 150, 150, 150} },
+                { Colors.Violet, new List<int> () { 150, 150, 150, 50} },
+                { Colors.LimeGreen, new List<int> () { 150, 50, 100, 100} },
+                { Colors.Brown, new List<int> () { 250, 50, 200, 100} },
+                { Colors.RosyBrown, new List<int> () { 250, 150, 200, 200} },
+                { Colors.RoyalBlue, new List<int> () { 150, 150, 100, 200} },
+                { Colors.Pink, new List<int> () { 100, 100, 200, 100} },
+                { Colors.DarkCyan, new List<int> () { 200, 100, 200, 200} },
+                { Colors.DarkGoldenrod, new List<int> () { 200, 200, 100, 200} },
+                { Colors.DarkBlue, new List<int> () { 100, 200, 100, 100} }
+
+
+            };
+
+
+            foreach (var item in dict)
+            {
+
+                foxDraw.SetStrokeColor(item.Key);
+                
+                foxDraw.DrawLine(item.Value[0], item.Value[1], item.Value[2], item.Value[3]);
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ week-02/day-03/LineInTheMiddle/DrawingApplication/DrawingApplication/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using GreenFox;
+
+namespace DrawingApplication
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+
+            //here
+            //foxDraw.DrawLine(50, 50, 100, 100);
+            // draw a red horizontal line to the canvas' middle.
+            // draw a green vertical line to the canvas' middle.
+
+            foxDraw.SetStrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, 300, 400, 300);
+
+            foxDraw.SetStrokeColor(Colors.Green);
+            foxDraw.DrawLine(400, 0, 400, 300);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
