d85346b01be5ed2a36bafaf2f556c9c3e21ae73f
Pavel Nebesky <pavel@nebesky.eu>
2019-09-18 14:44:30 +0200

    add empty DrawingApplication project

---
+++ week-02/day-03/DrawingApplication/DrawingApplication/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using GreenFox;
+
+namespace DrawingApplication
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+
+            //here
+            //foxDraw.DrawLine(50, 50, 100, 100);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
