e3e378a42ff538d1df3e4af31a7a83838e59bf72
Pavel Nebesky <pavel@nebesky.eu>
2019-09-19 15:22:19 +0200

    Last two Drawing exercises

---
+++ week-02/day-04/ConnectTheDots/DrawingApplication/DrawingApplication/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using GreenFox;
+using Avalonia.Media;
+using System.Collections.Generic;
+
+namespace DrawingApplication
+{
+    public class MainWindow : Window
+    {
+        public static void drawList(FoxDraw foxDraw, List<List<int>> listToDraw)
+        {
+            for (int i = 0; i < listToDraw.Count - 1; i++)
+            {
+                
+                foxDraw.DrawLine(listToDraw[i][0], listToDraw[i][1], listToDraw[i + 1][0], listToDraw[i + 1][1]);
+            }
+            foxDraw.DrawLine(listToDraw[0][0], listToDraw[0][1], listToDraw[listToDraw.Count-1][0], listToDraw[listToDraw.Count-1][1]);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            canvas.Width = 800;
+            canvas.Height = 600;
+
+            //here
+            //foxDraw.DrawLine(50, 50, 100, 100);
+
+            // Create a function that takes 2 parameters:
+            // A list of (x, y) points and foxDraw
+            // and connects them with green lines.
+            // connect these to get a box: {new Point(10, 10), new Point(290, 10), new Point(290, 290), new Point(10, 290)}
+            // Connect these: {new Point(50, 100), new Point(70, 70), new Point(80, 90), new Point(90, 90), new Point(100, 70),
+            // new Point(120, 100), new Point(85, 130), new Point(50, 100)}
+
+
+            List<List<int>> list1 = new List<List<int>>()
+            {
+                new List<int>() { 10, 10 },
+                new List<int>() { 290, 10 },
+                new List<int>() { 290, 290 },
+                new List<int>() { 10, 290 }
+            };
+
+            List<List<int>> list2 = new List<List<int>>()
+            {
+                new List<int>() { 50, 100 },
+                new List<int>() { 70, 70 },
+                new List<int>() { 80, 90 },
+                new List<int>() { 90, 90 },
+                new List<int>() { 100, 70 },
+                new List<int>() { 120, 100 },
+                new List<int>() { 85, 130 },
+                new List<int>() { 50, 100 }
+            };
+
+            drawList(foxDraw, list1);
+            drawList(foxDraw, list2);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ week-02/day-04/StarryNight/DrawingApplication/DrawingApplication/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using GreenFox;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+
+namespace DrawingApplication
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var canvas = this.Get<Canvas>("canvas");
+            var foxDraw = new FoxDraw(canvas);
+            canvas.Width = 800;
+            canvas.Height = 600;
+
+            //here
+            //foxDraw.DrawLine(50, 50, 100, 100);
+            // Draw the night sky:
+            //  - The background should be black
+            //  - The stars can be small squares
+            //  - The stars should have random positions on the canvas
+            //  - The stars should have random color (some shade of grey)
+
+            foxDraw.SetBackgroundColor(Colors.Black);
+
+            int randomX = 0;
+            int randomY = 0;
+            int randomColorIndex = 0;
+            int starSize = 5;
+
+            var listOfGreyColors = new List<Color>()
+                {   Colors.DarkGray,
+                    Colors.DarkSlateGray,
+                    Colors.DimGray,
+                    Colors.Gray,
+                    Colors.LightGray,
+                    Colors.LightSlateGray,
+                    Colors.SlateGray
+                };
+
+            var random = new Random();
+
+            for (int i = 0; i < 2000; i++)
+            {
+                randomX = random.Next(Convert.ToInt32(Width)-starSize);
+                randomY = random.Next(Convert.ToInt32(Height)-starSize);
+                randomColorIndex = random.Next(listOfGreyColors.Count-1);
+                foxDraw.SetFillColor(listOfGreyColors[randomColorIndex]);
+                foxDraw.DrawRectangle(randomX, randomY, starSize, starSize);
+            }
+            
+
+
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
