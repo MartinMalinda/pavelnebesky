cbc9b00085109c1b5277eca4697c501d7c69ecc5
Pavel Nebesky <pavel@nebesky.eu>
2019-09-24 15:03:17 +0200

    Reversed lines exercise

---
+++ week-03/day-02/ReversedLines/ReversedLines/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ReversedLines
+{
+    class Program
+    {
+
+        public static void Decode(string inputFile, string outputFile)
+        {
+            string inputLine = "";
+            string outputLine = "";
+            using (StreamReader reader = new StreamReader(inputFile))
+            {
+                while ((inputLine = reader.ReadLine()) != null)
+                {
+                    for (int i = inputLine.Length-1; i > -1; i--)
+                    {
+                        outputLine += inputLine[i];
+                    }
+                    outputLine += "\n";
+                    File.AppendAllText(outputFile, outputLine);
+                    outputLine = "";
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Decode("reversed-lines.txt", "non-reversed-lines.txt");
+        }
+    }
+}
