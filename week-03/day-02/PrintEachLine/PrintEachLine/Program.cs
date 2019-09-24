5cb2e9af969234b664c5f4af3610c262ba4303a4
Pavel Nebesky <pavel@nebesky.eu>
2019-09-24 12:06:09 +0200

    Print each line exercise

---
+++ week-03/day-02/PrintEachLine/PrintEachLine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Example
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Write a program that opens a file called "my-file.txt", then prints
+            // each line from the file.
+            // If the program is unable to read the file (for example it does not exist),
+            // then it should print the following error message: "Unable to read file: my-file.txt"
+
+            //string currentPath = Directory.GetCurrentDirectory();
+            //Console.WriteLine(currentPath);
+
+            string path = @"../../../my-file.txt";
+            try
+            {
+                string [] lines = File.ReadAllLines(path);
+                // Prints the first line of the file
+                foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }
+        }
+    }
+}
