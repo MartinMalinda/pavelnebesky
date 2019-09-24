2c24acb926a4cac574c072cb0452e5f99ebce56a
Pavel Nebesky <pavel@nebesky.eu>
2019-09-24 14:43:00 +0200

    Doubled exercise

---
+++ week-03/day-02/Doubled/Doubled/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Doubled
+{
+    class Program
+    {
+
+        // Create a method that decrypts the duplicated-chars.txt
+
+        public static void Decode(string inputFile, string outputFile)
+        {
+            string inputLine = "";
+            string outputLine = "";
+            using (StreamReader reader = new StreamReader(inputFile))
+            {
+                while ((inputLine = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < inputLine.Length; i = i + 2)
+                    {
+                        outputLine += inputLine[i];
+                    }
+                    outputLine += "\n";
+                    File.AppendAllText(outputFile, outputLine);
+                    outputLine = "";
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Decode("duplicated-chars.txt", "non-duplicated-chars.txt");
+        }
+    }
+}
