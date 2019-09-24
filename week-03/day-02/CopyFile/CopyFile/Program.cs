b554a8629e9fee607aff9e65fd32e32c5b4493c1
Pavel Nebesky <pavel@nebesky.eu>
2019-09-24 13:58:02 +0200

    Copy file exercise

---
+++ week-03/day-02/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFile
+{
+    class Program
+    {
+
+        // Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+        // It should take the filenames as parameters
+        // It should return a boolean that shows if the copy was successful
+
+        public static bool Copy (string inputPath, string outputPath)
+        {
+            bool success = false;
+            try
+            {
+                File.WriteAllText(outputPath, File.ReadAllText(inputPath));
+                success = true;
+            }
+            catch
+            {
+
+            }
+            return success;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Copy(@"./input.txt", @"./output.txt"));
+        }
+    }
+}
