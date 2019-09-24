5608396ac90fb983e03a9d8f935e08e1d076e676
Pavel Nebesky <pavel@nebesky.eu>
2019-09-24 15:20:00 +0200

    Reversed order exercise

---
+++ week-03/day-02/ReversedOrder/ReversedOrder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ReversedOrder
+{
+    class Program
+    {
+        public static void ReverseOrder(string inputFile, string outputFile)
+        {
+            string[] lines = File.ReadAllLines(inputFile);
+            for (int i = lines.Length - 1; i > -1; i--)
+                {
+                File.AppendAllText(outputFile, lines[i]+ "\n");
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            ReverseOrder("reversed-order.txt", "non-reversed-order.txt");
+        }
+    }
+}
