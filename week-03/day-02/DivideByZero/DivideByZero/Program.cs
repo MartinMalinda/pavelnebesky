04731b8b74d839bd5f0b4bf93332244ce93b8cec
Pavel Nebesky <pavel@nebesky.eu>
2019-09-24 11:43:27 +0200

    Divide by zero exercise

---
+++ week-03/day-02/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        // Create a function that takes a number
+        // divides ten with it,
+        // and prints the result.
+        // It should print "fail" if the parameter is 0
+
+        public static void TenDividedBy(int number)
+        {
+            try  // Prevents the program breaking when attempting dividing by zero
+            {
+                // If the input value for divisor was 0 the program breaks
+                int result = 10 / number;
+                // The program doesn't reach this line if the input was 0
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                // This line only runs if the input was 0
+                Console.WriteLine("fail");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            TenDividedBy(0);
+            TenDividedBy(5);
+        }
+    }
+}
