e82fa97555e753793d1830e29dfc736cb3762a6d
Pavel Nebesky <pavel@nebesky.eu>
2019-09-25 14:04:59 +0200

    Animal exercise

---
+++ week-03/day-03/Animal/Animal/EmptyClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Animal
+{
+
+//Create an Animal class
+//Every animal has a hunger value, which is a whole number
+//Every animal has a thirst value, which is a whole number
+//when creating a new animal object these values are created with the default 50 value
+//Every animal can Eat() which decreases their hunger by one
+//Every animal can Drink() which decreases their thirst by one
+//Every animal can Play() which increases both by one
+
+    public class Animal
+    {
+        public int Hunger { get; set; } = 50;
+        public int Thirst { get; set; } = 50;
+       
+        public Animal()
+        {
+        }
+
+        public Animal(int hunger, int thirst)
+        {
+            Hunger = hunger;
+            Thirst = thirst;
+        }
+
+        public void Eat()
+        {
+            Hunger--;
+        }
+
+        public void Drink()
+        {
+            Thirst--;
+        }
+
+        public void Play()
+        {
+            Hunger++;
+            Thirst++;
+        }
+
+    }
+}
---
+++ week-03/day-03/Animal/Animal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+
+        //Create an Animal class
+        //Every animal has a hunger value, which is a whole number
+        //Every animal has a thirst value, which is a whole number
+        //when creating a new animal object these values are created with the default 50 value
+        //Every animal can Eat() which decreases their hunger by one
+        //Every animal can Drink() which decreases their thirst by one
+        //Every animal can Play() which increases both by one
+
+        static void Main(string[] args)
+        {
+            var pig = new Animal();
+
+            pig.Eat();
+
+            Console.WriteLine(pig.Hunger);
+        }
+    }
+}
