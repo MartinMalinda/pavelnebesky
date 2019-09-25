1b8517d19fea07c2f310e44c4958eb8f8c8840a3
Pavel Nebesky <pavel@nebesky.eu>
2019-09-25 14:54:08 +0200

    Counter exercise

---
+++ week-03/day-03/Counter/Counter/EmptyClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Counter
+{
+
+    //Create Counter class
+    //which has an integer property
+    //when creating it should have a default value 0 or we can specify it when creating
+    //we can Add(number) to this counter another whole number
+    //or we can Add() without parameters just increasing the counter's value by one
+    //and we can Get() the current value as string
+    //also we can Reset() the value to the initial value
+
+    public class Counter
+    {
+        public int Number { get; set; } = 0;
+
+        public Counter()
+        {
+        }
+
+        public Counter(int number)
+        {
+            Number = number;
+        }
+
+        public void Add(int toAdd)
+        {
+            Number += toAdd;
+        }
+
+        public void Add()
+        {
+            Number += 1;
+        }
+
+        public string Get()
+        {
+            return Convert.ToString(Number);
+        }
+        public void Reset()
+        {
+            Number = 0;
+        }
+    }
+}
---
+++ week-03/day-03/Counter/Counter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+
+//Create Counter class
+//which has an integer property
+//when creating it should have a default value 0 or we can specify it when creating
+//we can Add(number) to this counter another whole number
+//or we can Add() without parameters just increasing the counter's value by one
+//and we can Get() the current value as string
+//also we can Reset() the value to the initial value
+
+        static void Main(string[] args)
+        {
+
+            var myNumber = new Counter();
+            myNumber.Add(10);
+            myNumber.Add();
+
+            Console.WriteLine(myNumber.Number);
+            string test = myNumber.Get();
+            Console.WriteLine(test);
+        }
+    }
+}
