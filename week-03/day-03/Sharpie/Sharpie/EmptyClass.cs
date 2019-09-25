8849f1d9fb542645de00bf63bb8433937d48a799
Pavel Nebesky <pavel@nebesky.eu>
2019-09-25 14:21:16 +0200

    Sharpie exercise

---
+++ week-03/day-03/Sharpie/Sharpie/EmptyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Sharpie
+{
+
+    //Create Sharpie class
+    //We should know about each sharpie their Color(which should be a string), Width(which will be a floating point number), InkAmount(another floating point number)
+    //When creating one, we need to specify the Color and the Width
+    //Every sharpie is created with a default 100 as InkAmount
+    //We can Use() the sharpie objects
+    //which decreases inkAmount
+
+    public class Sharpie
+    {
+        public string Color { get; set; }
+        public float Width { get; set; }
+        public float Inkamount { get; set; } = 100;
+
+
+        public Sharpie(string color, float width)
+        {
+            Color = color;
+            Width = width;
+        }
+
+        public void Use ()
+        {
+            Inkamount--;
+        }
+
+    }
+}
---
+++ week-03/day-03/Sharpie/Sharpie/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sharpie
+{
+    class Program
+    {
+
+        //Create Sharpie class
+        //We should know about each sharpie their Color(which should be a string), Width(which will be a floating point number), InkAmount(another floating point number)
+        //When creating one, we need to specify the Color and the Width
+        //Every sharpie is created with a default 100 as InkAmount
+        //We can Use() the sharpie objects
+        //which decreases inkAmount
+
+        static void Main(string[] args)
+        {
+            var mySharpie = new Sharpie("Blue", 10);
+
+            mySharpie.Use();
+
+            Console.WriteLine(mySharpie.Inkamount);
+        }
+    }
+}
