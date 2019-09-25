48ada1a8ae2cc66ebadb4f5867825df6df60f685
Pavel Nebesky <pavel@nebesky.eu>
2019-09-25 16:31:45 +0200

    Dice set exercise

---
+++ week-03/day-03/Dice/Dice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreenFoxAcademy
+{
+    class Program
+    {
+        public static void Main(string[] argv)
+        {
+            // You have a `DiceSet` class which has a list for 6 dice
+            // You can roll all of them with roll()
+            // Check the current rolled numbers with getCurrent()
+            // You can reroll with reroll()
+            // Your task is to roll the dice until all of the dice are 6
+
+            DiceSet diceSet = new DiceSet();
+
+            diceSet.Roll();
+
+            for (int i = 0; i < 6; i++)
+            {
+                while (diceSet.GetCurrent(i) !=6)
+                {
+                    diceSet.Reroll(i);
+                }
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(diceSet.GetCurrent(i));
+            }
+        }    
+    }
+}
