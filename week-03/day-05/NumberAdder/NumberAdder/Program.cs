d36bd8205ff594716b641bbbb1eb939c8bef16d2
Pavel Nebesky <pavel@nebesky.eu>
2019-09-27 11:33:36 +0200

    Number Adder exercise

---
+++ week-03/day-05/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberAdder
+{
+
+    //Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+
+    class Program
+    {
+
+        public static int AddNumbers(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n + AddNumbers(n - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number: ");
+            Console.WriteLine(AddNumbers(Convert.ToInt32(Console.ReadLine())));
+        }
+    }
+}
