38e345cf30c561a8e3115bc345b6ecf42aa96010
Pavel Nebesky <pavel@nebesky.eu>
2019-10-02 15:42:27 +0200

    Dominoes exercise

---
+++ week-04/day-03/Dominoes/Dominoes/Domino.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domino
+{
+    public class Domino : IComparable
+    {
+        private readonly int[] values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.values = new int[] { valueA, valueB };
+        }
+
+        public int CompareTo(object obj)
+        {
+         
+            var domino = obj as Domino;
+
+           
+
+                if (obj == null || values[0] > domino.values[0])
+                {
+                    return 10;
+                }
+                else if (values[0] == domino.values[0])
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -10;
+                }
+            
+        }
+
+        public int[] GetValues()
+        {
+            return values;
+        }
+
+    }
+}
