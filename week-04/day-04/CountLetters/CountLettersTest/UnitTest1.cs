941bb02a0462d200a48a0d824e830f65897a1475
Pavel Nebesky <pavel@nebesky.eu>
2019-10-03 15:14:01 +0200

    Count Letters test exercise

---
+++ week-04/day-04/CountLetters/CountLettersTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+using MostCommonCharacters;
+using System.Collections.Generic;
+
+namespace CountLettersTest
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            Dictionary<char, int> occurrencesTest = new Dictionary<char, int>();
+            string testString = "this is test string";
+            occurrencesTest.Add('t', 4);
+            occurrencesTest.Add('h', 1);
+            occurrencesTest.Add('i', 3);
+            occurrencesTest.Add('s', 4);
+            occurrencesTest.Add('e', 1);
+            occurrencesTest.Add(' ', 3);
+            occurrencesTest.Add('r', 1);
+            occurrencesTest.Add('n', 1);
+            occurrencesTest.Add('g', 1);
+
+            Assert.Equal(occurrencesTest, Program.toDictionary(testString));
+        }
+    }
+}
---
+++ week-04/day-04/CountLetters/MostCommonCharacters/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MostCommonCharacters
+{
+    public class Program
+    {
+        
+        //Write a function, that takes a string as an argument and returns a dictionary with all letters in the string as keys, and numbers as values that shows how many occurrences there are.
+        //Create a test for that.
+
+        public static Dictionary<char, int> toDictionary(string inputString)
+        {
+            Dictionary<char, int> occurrences = new Dictionary<char, int> ();
+            
+            foreach (var item in inputString)
+            {
+                if (occurrences.ContainsKey(item))
+                {
+                    occurrences[item]++;
+                }
+                else
+                {
+                    occurrences.Add(item, 1);
+                }
+            }
+
+            return occurrences;
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> occurrencesTest = new Dictionary<char, int>();
+            occurrencesTest = toDictionary("this is test string");
+            foreach (var item in occurrencesTest)
+            {
+                Console.WriteLine(item.Key + " : "+ item.Value);
+            }
+
+        }
+    }
+}
