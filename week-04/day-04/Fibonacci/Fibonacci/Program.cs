090e7bfe8f00443c7694ff1f56f950b0e7a54428
Pavel Nebesky <pavel@nebesky.eu>
2019-10-03 15:41:08 +0200

    Fibonacci test exercise

---
+++ week-04/day-04/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    public class Program
+    {
+
+        static Dictionary<int, double> fibonacciCache = new Dictionary<int, double>();
+
+        public static double Fibonacci(int n)
+        {
+            fibonacciCache.Clear();
+            CountFibonacciCache(n);
+            return CountFibonacci(n);
+
+        }
+
+        public static double CountFibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else if (fibonacciCache.ContainsKey(n-1) && (fibonacciCache.ContainsKey(n - 2)))
+            {
+                return fibonacciCache[n-1] + fibonacciCache[n - 2];
+            }
+            else if (fibonacciCache.ContainsKey(n - 2))
+            {
+                return CountFibonacci(n - 1) + fibonacciCache[n - 2];
+            }
+            else
+            {
+                return CountFibonacci(n - 1) + CountFibonacci(n - 2);
+            }
+        }
+
+        public static void CountFibonacciCache(int n)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+                fibonacciCache.Add(i, CountFibonacci(i));
+                Console.WriteLine("Cache: " + i + " " + fibonacciCache[i]);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int number = new int();
+            Console.Write("Give me a int number to count Fibonacci: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Fibonacci for " + number + " is: " + Fibonacci(number));
+            Console.WriteLine();
+        }
+    }
+}
---
+++ week-04/day-04/Fibonacci/FibonacciTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using Fibonacci;
+
+namespace FibonacciTest
+{
+    public class UnitTest1
+    {
+
+        //Write a function that computes a member of the fibonacci sequence by a given index
+        //Create tests for multiple test cases.
+
+        [Fact]
+        public void Test1()
+        {
+            Double fibonacci1 = 1;
+            Double fibonacci5 = 5;
+            Double fibonacci10 = 55;
+
+            Assert.Equal(fibonacci1, Program.Fibonacci(1));
+            Assert.Equal(fibonacci5, Program.Fibonacci(5));
+            Assert.Equal(fibonacci10, Program.Fibonacci(10));
+
+
+        }
+    }
+}
