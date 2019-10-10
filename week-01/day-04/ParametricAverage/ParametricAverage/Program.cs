28a6a37099cd6c399d8c73db694bce94e47f590d
Pavel Nebesky <pavel@nebesky.eu>
2019-10-10 11:01:22 +0200

    Parametric Average exam exercise

---
+++ week-01/day-04/ParametricAverage/ParametricAverage/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for a number.
+            // It would ask this many times to enter an integer,
+            // if all the integers are entered, it should print the sum and average of these
+            // integers like:
+            //
+            // Sum: 22, Average: 4.4
+
+            int numberOfNumbers = new int();
+
+            Console.WriteLine("Please give me number of numbers: ");
+            numberOfNumbers = Convert.ToInt32(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < numberOfNumbers; i++)
+            {
+                Console.WriteLine("Please give me " + (i+1) + ". number: ");
+                numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            int sumOfNumbers = 0;
+
+            foreach (var item in numbers)
+            {
+                sumOfNumbers += item;
+            }
+
+            double averageOfNumbers = 0;
+
+            averageOfNumbers = Convert.ToDouble(sumOfNumbers) / Convert.ToDouble(numbers.Count);
+
+            Console.WriteLine();
+            Console.WriteLine("Sum: " + sumOfNumbers + ", Average: " + averageOfNumbers);
+
+        }
+    }
+}
