16a1058b38e96ed46ed000f783e5c251bf49457c
Pavel Nebesky <pavel@nebesky.eu>
2019-10-21 16:08:53 +0200

    add LINQ Exercise 7 W6 D1

---
+++ week-06/day-01/Exercise7/Exercise7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise7
+{
+    class Program
+    {
+
+        //Write a LINQ Expression to find the strings which starts with A and ends with I in the following array:
+
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+
+            cities.Where(n => (n[0] == 'A' && n[cities.Length-1] == 'I')).ToList().ForEach(r => Console.WriteLine(r));
+
+            Console.WriteLine();
+
+            var resultQuery = from item in cities
+                              where (item[0] == 'A' && item[cities.Length - 1] == 'I')
+                              select item;
+
+            foreach (var item in resultQuery)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
