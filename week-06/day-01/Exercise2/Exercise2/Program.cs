8dc1bf12507283040ad20d406b057428c6d96b85
Pavel Nebesky <pavel@nebesky.eu>
2019-10-21 13:41:15 +0200

    LINQ Exercise 2 W6 D1

---
+++ week-06/day-01/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise2
+{
+    class Program
+    {
+
+        //Write a LINQ Expression to get the average value of the odd numbers from the following array:
+
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var result = n.Where(n => n % 2 == 1).Average();
+			Console.WriteLine(result);
+		}
+    }
+}
