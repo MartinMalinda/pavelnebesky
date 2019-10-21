a34f30e136c72f4527a55f8e48fb7f10bb571200
Pavel Nebesky <pavel@nebesky.eu>
2019-10-21 12:02:47 +0200

    add LINQ Exercise 1 W6 D1

---
+++ week-06/day-01/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise1
+{
+    class Program
+    {
+
+        // Write a LINQ Expression to get the even numbers from the following array:
+
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var result = n.Where(n => n % 2 == 0);
+            result.ToList().ForEach(r => Console.WriteLine(r));
+
+            
+        }
+    }
+}
