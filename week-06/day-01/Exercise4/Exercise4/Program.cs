e840b60c12fc897029178f28de8a023eba7f5f3a
Pavel Nebesky <pavel@nebesky.eu>
2019-10-21 14:05:52 +0200

    LINQ Exercise 4 W6 D1

---
+++ week-06/day-01/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise4
+{
+    class Program
+    {
+        //Write a LINQ Expression to find which number squared value is more then 20 from the following array:
+
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var result = n.Where(n => n*n > 20);
+            result.ToList().ForEach(r => Console.WriteLine(r));
+        }
+    }
+}
