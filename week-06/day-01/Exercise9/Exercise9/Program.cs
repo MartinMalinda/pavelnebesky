555a210b85fee32cdf8310b21de5ba48b3bc6cff
Pavel Nebesky <pavel@nebesky.eu>
2019-10-21 17:25:41 +0200

    add LINQ Exercise 9 W6 D1

---
+++ week-06/day-01/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise9
+{
+    class Program
+    {
+
+        //Write a LINQ Expression to convert a char array to a string.
+
+        static void Main(string[] args)
+        {
+            char[] chars = { 'a', 'b', 'c', 'd', 'A', 'B', 'C', 'D', 'q', 'w', 'e' };
+
+
+            string answerString = "";
+
+            chars.ToList().ForEach(ch => answerString = answerString.Insert(answerString.Length,ch.ToString()));
+            Console.WriteLine(answerString);
+
+            Console.WriteLine();
+
+            string answerStringQuery = "";
+
+            var resultQuery = (from ch in chars
+                                   
+                               select answerStringQuery = answerStringQuery.Insert(answerStringQuery.Length, ch.ToString())).ToList();
+
+                Console.WriteLine(answerStringQuery);
+            
+        }
+    }
+}
