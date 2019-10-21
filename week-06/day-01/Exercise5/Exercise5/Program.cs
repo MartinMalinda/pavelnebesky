3103c096be612b1342133476b7473eb964d2e6b1
Pavel Nebesky <pavel@nebesky.eu>
2019-10-21 14:36:37 +0200

    LINQ Exercise 5 W6 D1

---
+++ week-06/day-01/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise5
+{
+    class Program
+    {
+
+        //Write a LINQ Expression to find the frequency of numbers in the following array:
+
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+           
+            n.GroupBy(n => n).ToList().ForEach(r => Console.WriteLine(r.Key + ": " + r.Count()));
+            
+
+        }
+    }
+}
