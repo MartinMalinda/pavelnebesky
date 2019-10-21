5cde9062ee46446b1866ca5bf87ed3a52d89c556
Pavel Nebesky <pavel@nebesky.eu>
2019-10-21 16:38:31 +0200

    add LINQ Exercise 8 W6 D1

---
+++ week-06/day-01/Exercise8/Exercise8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise8
+{
+    class Program
+    {
+
+        //Write a LINQ Expression to find the uppercase characters in a string.
+
+        static void Main(string[] args)
+        {
+			string givenString = "This is the Pavels given string";
+
+
+            givenString.ToCharArray().Where(g => g == Char.ToUpper(g) && g != ' ').ToList().ForEach(r => Console.WriteLine(r));
+
+			Console.WriteLine();
+
+            var resultQuery = from item in givenString
+                              where item == Char.ToUpper(item) && item != ' '
+                              select item;
+
+            foreach (var item in resultQuery)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
