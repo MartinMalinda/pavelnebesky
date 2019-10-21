342b57533dff48abda34ec6090409a46a7b61ef0
Pavel Nebesky <pavel@nebesky.eu>
2019-10-21 15:46:22 +0200

    add LINQ Exercise 6 W6 D1

---
+++ week-06/day-01/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise6
+{
+
+    //Write a LINQ Expression to find the frequency of characters in a given string.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenString = "this is the given string";
+
+
+            givenString.ToCharArray().GroupBy(n => n).ToList().ForEach(r => Console.WriteLine(r.Key + ": " + r.Count()));
+
+            Console.WriteLine();
+
+            var resultQuery = from item in givenString
+                              group item by item;
+
+            foreach (var item in resultQuery)
+            {
+                Console.WriteLine(item.Key + ": " + item.Count());
+            }
+        }
+    }
+}
