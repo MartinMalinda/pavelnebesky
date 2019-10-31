3693799515cfbedcbc9c1d0ff26088fdfd6e96a9
Pavel Nebesky <pavel@nebesky.eu>
2019-10-31 13:08:52 +0100

    add - HelloDIProject - W7 - D4

---
+++ week-07/day-04/HelloDIProject/HelloDIProject/ConsoleLoggerMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloDIProject
+{
+    class ConsoleLoggerMiddleware : IMiddleware
+    {
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Console.WriteLine("Hi from the middleware");
+
+            return next(context);
+        }
+
+        public ConsoleLoggerMiddleware(Printer printer)
+        {
+            printer.Log("Hi. I am lost.");
+        }
+    }
+}
---
+++ week-07/day-04/HelloDIProject/HelloDIProject/Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HelloDIProject
+{
+    public class Printer
+    {
+
+        public void Log(string message)
+        {
+            Console.WriteLine(" --- " + DateTime.Now + " MY PRINTER SAYS --- " + message);
+        }
+
+        //public Printer()
+        //{
+        //}
+    }
+}
