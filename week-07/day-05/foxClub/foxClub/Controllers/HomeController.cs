1248682f39f40efb592f64335233f958bbf38e30
Pavel Nebesky <pavel@nebesky.eu>
2019-11-01 14:08:25 +0100

    add - Fox Club - W7 - D5

---
+++ week-07/day-05/foxClub/foxClub/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using foxClub.Models;
+using foxClub.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace foxClub.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+
+        FoxServices foxServices;
+
+        public HomeController(FoxServices foxServices)
+        {
+            this.foxServices = foxServices;
+        }
+
+        // GET: /<controller>/
+        //[Route("home")]
+        public IActionResult Index(string name)
+        {
+            Fox selectedFox = foxServices.foxes.Find(f => f.Name == name);
+
+            return View(selectedFox);
+        }
+
+        [Route("login")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login(string name)
+        {
+            foxServices.foxes.Add(new Fox(name));
+            //Response.Redirect("/?name="+name);
+            return RedirectToAction("Index", "Home", new { name = name }); //anonymous object
+        }
+
+    }
+}
---
+++ week-07/day-05/foxClub/foxClub/Services/FoxServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using foxClub.Models;
+
+namespace foxClub.Services
+{
+    public class FoxServices
+    {
+
+        public List<Fox> foxes;
+
+        public FoxServices()
+        {
+            foxes = new List<Fox>();
+        }
+    }
+}
